97de20cfcbf46a7d9902e6e060cf97d2c3c44255
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 09:52:50 -0600

    Added game option variables

--- src/main/java/main/java/xyz/marshalldev/GameHandler/Game.java
+++ src/main/java/main/java/xyz/marshalldev/GameHandler/Game.java
@@ -1,5 +1,21 @@
 package main.java.xyz.marshalldev.GameHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Game {
 
+    private final int MAX_PLAYERS = 9;
+    private int numPlayers;
+    private final Map<Integer, Player> players = new HashMap<>();
+
+    private int startingBalance;
+
+    private int bigBlind;
+    private int littleBlind;
+    private int bigBlindIndex;
+    private int littleBlindIndex;
+    private int buttonIndex;
+
+    private int potValue;
 }
