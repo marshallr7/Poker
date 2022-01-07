7b2dc013e13ee5fb347cbd592dbc5c46eacca2e4
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 07:09:27 -0600

    Added toString method

--- src/main/java/xyz/marshalldev/CardHandler/Deck.java
+++ src/main/java/xyz/marshalldev/CardHandler/Deck.java
@@ -12,4 +12,11 @@ public class Deck {
             Suit.stream().forEach(suit -> deck.add(new Card(String.valueOf(finalI), suit)));
         }
     }
+
+    @Override
+    public String toString() {
+        final String[] cards = {"Deck: "};
+        deck.forEach(card -> cards[0] = cards[0] + card.toString() + " ");
+        return cards[0];
+    }
 }
