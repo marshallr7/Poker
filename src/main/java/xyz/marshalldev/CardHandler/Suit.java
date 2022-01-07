3b184dc7648cd212bb11c38afa03df9d5c7144bb
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 06:05:00 -0600

    Filled in enum information for cards suits and created constructor.

--- src/main/java/xyz/marshalldev/CardHandler/Suit.java
+++ src/main/java/xyz/marshalldev/CardHandler/Suit.java
@@ -1,6 +1,15 @@
 package main.java.xyz.marshalldev.CardHandler;
 
 public enum Suit {
+    SPADE("♠"),
+    CLUB("♣"),
+    HEART("♥"),
+    DIAMOND("♦");
 
+    public final String symbol;
+
+    Suit(String symbol) {
+        this.symbol = symbol;
+    }
 
 }
