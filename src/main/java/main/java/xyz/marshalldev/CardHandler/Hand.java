f8f79d86e66ec19d6c14838d7e1e77dddeae5ee2
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 09:27:16 -0600

    Added @data annotation and storage for list of cards. Added addCard method

--- src/main/java/main/java/xyz/marshalldev/CardHandler/Hand.java
+++ src/main/java/main/java/xyz/marshalldev/CardHandler/Hand.java
@@ -1,4 +1,16 @@
 package main.java.xyz.marshalldev.CardHandler;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Hand {
+
+    List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
 }
