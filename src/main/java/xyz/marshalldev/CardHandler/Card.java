79c11917d203a6c2abbc4475e4b7663e9928b73c
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 06:30:52 -0600

    Added constructor, getters, and setters for a Card.

--- src/main/java/xyz/marshalldev/CardHandler/Card.java
+++ src/main/java/xyz/marshalldev/CardHandler/Card.java
@@ -5,4 +5,25 @@ public class Card {
     String value;
     Suit suit;
 
+    public Card(String value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
 }
