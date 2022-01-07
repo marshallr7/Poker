b666ee8485fb85fa3187f08c116cd25bf1639487
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 14:17:51 -0600

    Added isAllIn method and current pot variables

--- src/main/java/main/java/xyz/marshalldev/GameHandler/Player.java
+++ src/main/java/main/java/xyz/marshalldev/GameHandler/Player.java
@@ -5,7 +5,15 @@ import main.java.xyz.marshalldev.CardHandler.Hand;
 
 @Data
 public class Player {
+    // Base info
     private int balance;
     private final Hand hand = new Hand();
 
+    // Current pot info
+    private int currentBet;
+    private boolean folded;
+
+    private boolean isAllIn() {
+        return this.balance == 0 && !folded;
+    }
 }
