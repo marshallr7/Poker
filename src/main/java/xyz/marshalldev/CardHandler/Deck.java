e30d2a6654a7c2b289eaabd4135c7759faee26bd
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 07:00:20 -0600

    Added generate method to build a deck

--- src/main/java/xyz/marshalldev/CardHandler/Deck.java
+++ src/main/java/xyz/marshalldev/CardHandler/Deck.java
@@ -6,4 +6,10 @@ public class Deck {
 
     ArrayList<Card> deck = new ArrayList<>();
 
+    public void generate() {
+        for (int i = 1; i < 14; i++) {
+            int finalI = i;
+            Suit.stream().forEach(suit -> deck.add(new Card(String.valueOf(finalI), suit)));
+        }
+    }
 }
