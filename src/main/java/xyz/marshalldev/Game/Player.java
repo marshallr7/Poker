1d8270cf3ff3cd87cad8d4e19c803cb0b55ca800
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 08:34:16 -0600

    Hand and balance getter and setters

--- src/main/java/xyz/marshalldev/Game/Player.java
+++ src/main/java/xyz/marshalldev/Game/Player.java
@@ -5,4 +5,20 @@ import main.java.xyz.marshalldev.CardHandler.Hand;
 public class Player {
     private int balance;
     private Hand hand;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
 }
