f634e2eb62c4374f5dbb6e81545da5dd4082d1ed
Marshall Patterson <marspatt@ttu.edu>
2022-01-07 09:53:14 -0600

    Changed to Lombok data class

--- src/main/java/main/java/xyz/marshalldev/GameHandler/Game.java
+++ src/main/java/main/java/xyz/marshalldev/GameHandler/Game.java
@@ -1,8 +1,11 @@
 package main.java.xyz.marshalldev.GameHandler;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Data
 public class Game {
 
     private final int MAX_PLAYERS = 9;
