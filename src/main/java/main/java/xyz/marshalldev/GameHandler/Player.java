71fe391e9965b54e5c4b86d1c22472287b33ed41
Marshall Patterson <marspatt@ttu.edu>
2022-01-11 20:02:18 -0600

    Added getAction, updateBalance, scan and status variables

--- src/main/java/main/java/xyz/marshalldev/GameHandler/Player.java
+++ src/main/java/main/java/xyz/marshalldev/GameHandler/Player.java
@@ -3,21 +3,36 @@ package main.java.xyz.marshalldev.GameHandler;
 import lombok.Data;
 import main.java.xyz.marshalldev.CardHandler.Hand;
 
+import java.util.Scanner;
+
 @Data
 public class Player {
+
+    private Scanner scan = new Scanner(System.in);
+
     // Base info
     private int balance;
     private final Hand hand = new Hand();
 
     // Current pot info
     private int currentBet;
-    private boolean folded;
+    private Action status;
 
-    private boolean isAllIn() {
-        return this.balance == 0 && !folded;
+    boolean isAllIn() {
+        return this.balance == 0 && status != Action.FOLD;
     }
 
-    private boolean isActive() {
-        return this.balance == 0 && !folded;
+    boolean isActive() {
+        return this.balance != 0 && status != Action.FOLD;
+    }
+
+    char getAction(int playerNumber) {
+        System.out.println("Player " + (playerNumber+1) + ", these are your cards: " + hand.toString() + "\nWould you like to bet, check, or fold? ");
+        String option = scan.nextLine();
+        return Character.toLowerCase(option.charAt(0));
+    }
+
+    void updateBalance(int amount) {
+        setBalance(getBalance() + amount);
     }
 }
