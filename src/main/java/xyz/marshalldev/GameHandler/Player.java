19485366ae64fd70ff3f152d5f0b1a1afb005ae6
Marshall <mryanp7@gmail.com>
2022-01-24 15:18:12 -0600

    Changed getAction to return Action instead of char

--- src/main/java/xyz/marshalldev/GameHandler/Player.java
+++ src/main/java/xyz/marshalldev/GameHandler/Player.java
@@ -26,10 +26,21 @@ public class Player {
         return this.balance != 0 && status != Action.FOLD;
     }
 
-    char getAction(int playerNumber) {
-        System.out.println("Player " + (playerNumber+1) + ", these are your cards: " + hand.toString() + "\nWould you like to bet, check, or fold? ");
+    Action getAction(int playerNumber) {
+        System.out.println("Player " + (playerNumber+1) + ", these are your cards: " + hand + "\nWould you like to check, bet, call, or fold? ");
         String option = scan.nextLine();
-        return Character.toLowerCase(option.charAt(0));
+
+        switch (Character.toLowerCase(option.charAt(0))) {
+            case 'b':
+                return Action.BET;
+            case 'c':
+                return Action.CHECK;
+            case 'f':
+                return Action.FOLD;
+            default:
+                System.out.println("Player " + (playerNumber+1) + ", please enter a valid action. (Check, Bet, Call, Fold)");
+                return getAction(playerNumber);
+        }
     }
 
     void updateBalance(int amount) {
