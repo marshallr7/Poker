bcd7a7ac469d73c3db1394aa12bb8d248be1ee54
Marshall <marspatt@ttu.edu>
2022-01-26 11:32:11 -0600

    Moved getAction method from Player class to Action

--- src/main/java/xyz/marshalldev/GameHandler/Player.java
+++ src/main/java/xyz/marshalldev/GameHandler/Player.java
@@ -26,23 +26,6 @@ public class Player {
         return this.balance != 0 && status != Action.FOLD;
     }
 
-    Action getAction(int playerNumber) {
-        System.out.println("Player " + (playerNumber+1) + ", these are your cards: " + hand + "\nWould you like to check, bet, call, or fold? ");
-        String option = scan.nextLine();
-
-        switch (Character.toLowerCase(option.charAt(0))) {
-            case 'b':
-                return Action.BET;
-            case 'c':
-                return Action.CHECK;
-            case 'f':
-                return Action.FOLD;
-            default:
-                System.out.println("Player " + (playerNumber+1) + ", please enter a valid action. (Check, Bet, Call, Fold)");
-                return getAction(playerNumber);
-        }
-    }
-
     void updateBalance(int amount) {
         setBalance(getBalance() + amount);
     }
