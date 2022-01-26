bd9b9ae45d0460f643ee70d6e58963217cfd9935
Marshall <marspatt@ttu.edu>
2022-01-26 11:31:14 -0600

    Added ALL_IN enum, added betActions list, moved getAction and modified code to display a GUI instead of command line

--- src/main/java/xyz/marshalldev/GameHandler/Action.java
+++ src/main/java/xyz/marshalldev/GameHandler/Action.java
@@ -1,8 +1,38 @@
 package xyz.marshalldev.GameHandler;
 
+import xyz.marshalldev.GUI;
+
 public enum Action {
     BET,
     CHECK,
     FOLD,
-    CALL
+    CALL,
+    ALL_IN;
+
+    static final String[] betActions = {
+            "Check",
+            "Bet",
+            "Fold",
+            "Call",
+            "All In"
+    };
+
+    public static Action getAction(Player player) {
+        int result = GUI.template(
+                "What would you like to do?",
+                "Cards: " + player.getHand().toString() + " - Balance: $" + player.getBalance(),
+                betActions);
+
+        return switch (result) {
+            case 0 -> Action.CHECK;
+            case 1 -> Action.BET;
+            case 2 -> Action.FOLD;
+            case 3 -> Action.CALL;
+            case 4 -> Action.ALL_IN;
+            default -> getAction(player);
+        };
+
+    }
 }
+
+
