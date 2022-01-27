4eadb93f4fb929f4341b169b5a6a74892cc8bf65
Marshall <mryanp7@gmail.com>
2022-01-26 22:15:57 -0600

    dialogTemplate method

--- src/main/java/xyz/marshalldev/GUI.java
+++ src/main/java/xyz/marshalldev/GUI.java
@@ -4,9 +4,13 @@ import javax.swing.*;
 
 public class GUI {
 
-    public static int template(String message, String title, Object[] options) {
+    public static int buttonTemplate(String message, String title, Object[] options) {
         return JOptionPane.showOptionDialog(null, message, title,
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                 null, options, options[0]);
+    }
+
+    public static String dialogTemplate(String message, String title) {
+        return JOptionPane.showInputDialog(message, title, JOptionPane.PLAIN_MESSAGE);
     }
 }
