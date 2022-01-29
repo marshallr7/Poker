7693d56055d3e89396c39624436a4bdafde711aa
Marshall <marspatt@ttu.edu>
2022-01-29 01:54:55 -0600

    Added int value to suit for pairing/sorting

--- src/main/java/xyz/marshalldev/CardHandler/Suit.java
+++ src/main/java/xyz/marshalldev/CardHandler/Suit.java
@@ -1,17 +1,22 @@
 package xyz.marshalldev.CardHandler;
 
+import lombok.Getter;
+
 import java.util.stream.Stream;
 
+@Getter
 public enum Suit {
-    SPADE("♠"),
-    HEART("♥"),
-    DIAMOND("♦"),
-    CLUB("♣");
+    SPADE("♠", 0),
+    HEART("♥", 1),
+    DIAMOND("♦", 2),
+    CLUB("♣", 3);
 
     public final String symbol;
+    public final int rank;
 
-    Suit(String symbol) {
+    Suit(String symbol, int rank) {
         this.symbol = symbol;
+        this.rank = rank;
     }
 
     public static Stream<Suit> stream() {
