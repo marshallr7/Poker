4b888bfd929cc05917f2eebd4508473204002c94
Marshall <marspatt@ttu.edu>
2022-01-29 01:55:23 -0600

    Test

--- src/main/java/xyz/marshalldev/Main.java
+++ src/main/java/xyz/marshalldev/Main.java
@@ -1,11 +1,15 @@
 package xyz.marshalldev;
 
+import xyz.marshalldev.CardHandler.Rankings;
 import xyz.marshalldev.GameHandler.Game;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game(2, 1000, 0, 1, 2);
+//        Game game = new Game(2, 1000, 0, 1, 2);
+//        System.out.println(game.getDeck().toString());
+//        System.out.println(game.getDeck().size());
+        new Test();
     }
 }
