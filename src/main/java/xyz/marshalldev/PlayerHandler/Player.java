d8c6d32a1a00c047db2dd2811d48ecae941d1330
Marshall <marspatt@ttu.edu>
2022-02-04 01:36:18 -0600

    Initialized hand, added comments to variables, and added rank variable. Added toString method, and modified addBet

--- src/main/java/xyz/marshalldev/PlayerHandler/Player.java
+++ src/main/java/xyz/marshalldev/PlayerHandler/Player.java
@@ -2,6 +2,7 @@ package xyz.marshalldev.PlayerHandler;
 
 import lombok.Data;
 import xyz.marshalldev.CardHandler.Hand;
+import xyz.marshalldev.CardHandler.Rank;
 import xyz.marshalldev.GameHandler.Action;
 
 import java.util.Scanner;
@@ -11,13 +12,14 @@ public class Player {
 
     private Scanner scan = new Scanner(System.in);
 
-    // Base info
-    private int balance;
-    private final Hand hand = new Hand();
+    private int balance;                    // Money balance
+    private Hand hand = new Hand();         // Hand
 
     // Current pot info
-    private int currentBet;
-    private Action status;
+    private int currentBet;                 // Amount of money in current pot
+    private Action status;                  // If player is folded or all in
+
+    private Rank rank;                      // Player showdown value
 
     public boolean isAllIn() {
         return this.balance == 0 && status != Action.FOLD;
@@ -35,7 +37,13 @@ public class Player {
         setBalance(getBalance() - amount);
     }
 
-    public void addBetAmount(int amount) {
+    public void addBet(int amount) {
         setCurrentBet(getCurrentBet() + amount);
+        setBalance(getBalance() - amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Balance: $" + balance + "\nHand: " + hand.toString() + "\nCurrent bet: $" + currentBet + "\nStatus: " + status + "\nRank: " + rank;
     }
 }
