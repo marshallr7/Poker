09d0728c54149cec918fefd3978cb0d94e7da9c8
Marshall <marspatt@ttu.edu>
2022-02-08 01:22:34 -0600

    Hashmap for player - bet amount storage

--- src/main/java/xyz/marshalldev/GameHandler/Pot.java
+++ src/main/java/xyz/marshalldev/GameHandler/Pot.java
@@ -3,12 +3,17 @@ package xyz.marshalldev.GameHandler;
 import lombok.Getter;
 import xyz.marshalldev.PlayerHandler.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pot {
 
     @Getter
-    int value;
+    int value;                                      // Total pot value
     @Getter
-    int amountPerPlayer;
+    int amountPerPlayer;                            // Amount each player has bet
+
+    Map<Player, Integer> bets = new HashMap<>();    // Player - Bet amount, used to manage payouts
 
     void updatePot(int amount) {
         this.value = amount + this.value;
